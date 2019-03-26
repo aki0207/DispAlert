16e19b423acbb9cbf09d2746293638b11e930b6c
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-26 17:15:31 +0900

    アラート

--- DispAlert/ViewController.swift
+++ DispAlert/ViewController.swift
@@ -1,20 +1,26 @@
-//
-//  ViewController.swift
-//  DispAlert
-//
-//  Created by ききょ on 2019/03/26.
-//  Copyright © 2019 kikyo. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
+    @IBAction func dispButtonPushed(_ sender: Any) {
+        
+        let title = "アラートを表示"
+        let message = "カモーンぬ"
+        let ok_text = "ok"
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: ok_text, style: UIAlertAction.Style.cancel, handler: nil)
+        alert.addAction(okButton)
+        
+        present(alert,animated: true, completion: nil)
+        
+    }
 }
 
