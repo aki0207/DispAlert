7980f37960dbd099f68b4c2dc7c83e231b514ddd
aki0207 <forever.look.aki@docomo.ne.jp>
2019-03-27 12:54:30 +0900

    okボタン押下後画面遷移するのも作成

---
+++ DispAlert/SubViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class SubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+}
--- DispAlert/ViewController.swift
+++ DispAlert/ViewController.swift
@@ -1,14 +1,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    
     @IBAction func dispButtonPushed(_ sender: Any) {
         
         let title = "アラートを表示"
@@ -22,5 +22,32 @@ class ViewController: UIViewController {
         present(alert,animated: true, completion: nil)
         
     }
+    
+    @IBAction func transitionButton(_ sender: Any) {
+        
+        let title = "こっちはなんと"
+        let message = "画面遷移しちゃいまーす"
+        let ok_text = "let'sらgo"
+        
+
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        //OKボタン追加
+        let okAction = UIAlertAction(title: ok_text, style: UIAlertAction.Style.default, handler:{(action: UIAlertAction!) in
+            
+            //アラートが消えるのと画面遷移が重ならないように0.5秒後に画面遷移するようにしてる
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                // 0.5秒後に実行したい処理
+                self.performSegue(withIdentifier: "toSub", sender: nil)
+            }
+        }
+        )
+        
+        alert.addAction(okAction)
+        
+        //アラートを表示する
+        present(alert, animated: true, completion: nil)
+
+    }
 }
+
 
